45af3eb344b4012f7e305f2ebf6397fd2bb397f7
didacflorensa <didacflorensacazorla@gmail.com>
2022-06-06 19:32:38 +0200

    Añadir comentarios. Dídac

--- app/src/main/java/com/example/exemple_aplicaciones/SecondScreenShot.java
+++ app/src/main/java/com/example/exemple_aplicaciones/SecondScreenShot.java
@@ -11,13 +11,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.example.exemple_aplicaciones.databinding.ActivityMainBinding;
+import com.example.exemple_aplicaciones.databinding.SecondScreenBinding;
 import com.example.exemple_aplicaciones.viewmodel.MainViewModel;
 import com.example.exemple_aplicaciones.viewmodel.SecondViewModel;
 
-public class SecondScreenShot extends Activity {
+// TODO @Didac. En la siguiente linea es lo que tenias. Hay que extender AppCombatActivity
+// public class SecondScreenShot extends Activity
+public class SecondScreenShot extends AppCompatActivity  {
 
     public String TAG = "SecondScreen";
     public TextView texto;
@@ -26,7 +31,15 @@ public class SecondScreenShot extends Activity {
     public  int coins;
 
     //------------------------------------
-    public ActivityMainBinding binding;     // Se debe de poner el Binding del secondScreen
+    //TODO  @Didac Aqui tienes que referenciar el binding del xml, el cual te lo genera solo. Ahora mismo te da error porque el xml tiene otro binding
+    //
+    //TODO @Didac El Main binding forma parte de l'activity MainActivity. Dejo comentado este y pongo el nuevo
+
+    //TODO@Didac. Lo otro estaba todo bien
+
+    public SecondScreenBinding secondScreenBinding;
+
+    //public ActivityMainBinding binding;     // Se debe de poner el Binding del secondScreen
     public SecondViewModel model;
     //------------------------------------
 
@@ -39,16 +52,16 @@ public class SecondScreenShot extends Activity {
         //--------------------------------------------------------------------------------------------
 
         // @Jordi: Bind the xml with the activity (ActivityLevelsBinding is auto generated).
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        secondScreenBinding = SecondScreenBinding.inflate(getLayoutInflater());
 
         // Set the Content of the xml to the view
-        setContentView(binding.getRoot());
+        setContentView(secondScreenBinding.getRoot());
 
         // Set the viewModel
         model = new ViewModelProvider(this).get(SecondViewModel.class);
 
-        binding.setMainViewModel(model);
-        binding.setLifecycleOwner(this);
+        secondScreenBinding.setSecondViewModel(model);
+        secondScreenBinding.setLifecycleOwner(this);
 
         //--------------------------------------------------------------------------------------------
 
